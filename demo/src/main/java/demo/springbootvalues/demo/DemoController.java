139ac46d787087ee7be2137160fb0e02677869dd
choisungwook <kgg1959@naver.com>
2023-11-20 22:16:12 +0900

    KAN-36 Add springboot demo application

---
+++ demo/src/main/java/demo/springbootvalues/demo/DemoController.java
@@ -0,0 +1,19 @@
+package demo.springbootvalues.demo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+    @Value("${message1}")
+    private String message1;
+
+    @Value("${message2}")
+    private String message2;
+
+    @GetMapping("/")
+    public String getMessages() {
+        return message1 + " " + message2;
+    }
+}
